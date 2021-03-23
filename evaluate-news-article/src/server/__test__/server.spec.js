44a0fd5bfd08f0c4aee35575970100156561e432
Sara  El-bedeawi <selbedeawi@gmail.com>
2021-03-23 16:02:00 +0200

    Update server.spec.js

--- evaluate-news-article/src/server/__test__/server.spec.js
+++ evaluate-news-article/src/server/__test__/server.spec.js
@@ -1,10 +1,18 @@
-// to solve ReferenceError: regeneratorRuntime is not defined
-// https://knowledge.udacity.com/questions/174638
 import 'babel-polyfill'
+const request = require('supertest')
+const { app } = require('../index');
 
-describe('Server Test', () => {
-    // TODO: add your test cases to test server
-    // HINT: Review
-    //  1. https://www.npmjs.com/package/supertest
-    //  2. https://dev.to/nedsoft/testing-nodejs-express-api-with-jest-and-supertest-1km6
+const request = require('supertest')
+const app = require('../server')
+describe('Post Endpoints', () => {
+  it('should create a new post', async () => {
+    const res = await request(app)
+      .post('/api/posts')
+      .send({
+        userId: 1,
+        title: 'test is cool',
+      })
+    expect(res.statusCode).toEqual(201)
+    expect(res.body).toHaveProperty('post')
+  })
 })
