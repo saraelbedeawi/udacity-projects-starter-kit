42d7f85c2927db9f0fcefcf460f9239a7edfd0ea
Sara  El-bedeawi <selbedeawi@gmail.com>
2021-03-24 16:55:26 +0200

    Update server.spec.js

--- evaluate-news-article/src/server/__test__/server.spec.js
+++ evaluate-news-article/src/server/__test__/server.spec.js
@@ -2,7 +2,6 @@ import 'babel-polyfill'
 const { app } = require('../index');
 
 const request = require('supertest')
-const app = require('../server')
 describe('Post Endpoints', () => {
   it('should create a new post', async () => {
     const res = await request(app)
